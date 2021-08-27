1f8a988af05f9c561c921466020809626cdaf1cd
jenishjk <jenishsonijk@gmail.com>
2021-08-28 01:35:21 +0530

    project initiated

---
+++ main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# User:-@harshsoni_08
+
+
+from bot import Bot
+
+Bot().run()
---
+++ script.py
@@ -0,0 +1,32 @@
+class script(object):
+
+
+    START_MSG = """ <b>Hi {}
+
+You can call this as an Auto Filter Bot if you like :D
+
+If you want to use this bot Dm @harshsoni_08 
+❗️❗️Note:- ONLY PAID USE OF THIS BOT❗️❗️
+</b>"""
+
+
+    HELP_MSG = """<b>You will not get any help here😁</b>
+
+
+<b>Bot Commands - Works in Group only</b>
+(You need to be a Auth User in order to use these commands)
+
+this Bot will automatically search for your files and give links to user😁
+
+
+<b>For PAID use Dm:- @harshsoni_08</b> """
+
+
+    ABOUT_MSG = """
+⭕️<b>Creater :</b> @harshsoni_08
+
+⭕️<b>Language :</b> <code>Python3</code>
+
+⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>  
+
+"""
