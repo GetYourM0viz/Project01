9d559f51e10289208b956b83222189a00a6e9df9
Harshsoni <87391837+GetYourM0viz@users.noreply.github.com>
2021-09-29 11:48:53 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,16 +2,11 @@ class script(object):
 
 
     START_MSG = """ <b>HI DEAR💕{}💕 WELCOME😉
-THIS BOT IS
+THIS BOT IS BELONGS TO 
 
-<a href='https://t.me/REQUEST_M0viz/'> 🔥𝐑𝐄𝐐𝐔𝐄𝐒𝐓_𝐌𝟎𝐯𝐢𝐳 </a> _Official Bot🔥
-
-</b>"""
-
-
+ <a href='https://t.me/REQUEST_M0viz/'>𝐑𝐄𝐐𝐔𝐄𝐒𝐓_𝐌𝟎𝐯𝐢𝐳</a></b>"""
 
     HELP_MSG = """<b>You will not get any help here😁</b>
-
 
 <b>Bot Commands Works in Group only
       YOU NEED TO TAKE PERMISSION
@@ -20,16 +15,12 @@ THIS BOT IS
 <b>this Bot will automatically search 
       for your files
 and give links to user😁</b>
+<b>For PAID use MSG : <a href='https://t.me/harshsoni_08/'>𝘽𝙊𝙏 𝙊𝙒𝙉𝙀𝙍</a></b> """
 
-<b>For PAID use MSG : <a href='https://t.me/harshsoni_08/'>𝘽𝙊𝙏 𝙊𝙒𝙉𝙀𝙍
-
- </a></b> """
 
 
     ABOUT_MSG = """
-⭕️<b>Creater :<a href='https://t.me/harshsoni_08/'>𝚄𝙽𝙺𝙽𝙾𝚆𝙽 𝙽𝚄𝙼𝙱𝙴𝚁 </a>
-
-</b>
+⭕️<b>Creater :<a href='https://t.me/harshsoni_08/'>𝚄𝙽𝙺𝙽𝙾𝚆𝙽 𝙽𝚄𝙼𝙱𝙴𝚁 </a></b>
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
